74455c89bbda474008cdb499f290b08dbe3c959d
peter <2530434216@qq.com>
2020-09-06 14:38:22 +0800

    Revert "创建git项目"
    This reverts commit 4458c7dbd52182d9eea1a949816933dddd94330b.

--- app/src/main/java/com/huapintang/cashregister/ui/main/view/MainHomeView.java
+++
@@ -1,9 +0,0 @@
-package com.huapintang.cashregister.ui.main.view;
-
-/**
- * Created by cbp on 3/5 0005.
- */
-public interface MainHomeView {
-
-
-}
--- app/src/main/java/com/huapintang/cashregister/ui/welcome/model/WelcomeListener.java
+++
@@ -1,9 +0,0 @@
-package com.huapintang.cashregister.ui.welcome.model;
-
-/**
- * Created by cbp on 3/5 0005.
- */
-public interface WelcomeListener {
-    void intentLogin();
-    void intentMain();
-}
